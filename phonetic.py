da0c7e4dbf3c73d03fc1897baa0078f8a9e51826
b39070566 <b39070566@gmail.com>
2024-06-17 16:18:56 +0800

    Update phonetic.py

--- phonetic.py
+++ phonetic.py
@@ -12,7 +12,8 @@ def read(word):
         phones = row.find_all('code')
         phone = [e.text for e in phones]
         s = " ".join( phone )
+        word = (f"{chinese} ==> {s}")
     # s = row.find('sub')
-        return TextSendMessage(text= f"{chinese} ==> {s}" )
+        return TextSendMessage(text=word)
     except:
         return TextSendMessage(text="查無此字")
